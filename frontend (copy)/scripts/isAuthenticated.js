b93bbe4c5a41a4e090bd5365ae3d03b4edd9c2b1
yasuo-00 <matheusyt@hotmail.com>
2021-06-22 00:01:14 -0300

    React Refactoring

--- frontend (copy)/scripts/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', isAuthenticated);
-
-//verifica se usuario esta autenticado (logado)
-function isAuthenticated(){
-    if(sessionStorage.getItem('user')=== null){
-        window.location.replace('http://localhost:3333/');
-    }
-}
