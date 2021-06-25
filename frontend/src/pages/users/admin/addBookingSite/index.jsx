a208bbad51e7147d6dbf1de605d0d1b171e624ad
yasuo-00 <matheusyt@hotmail.com>
2021-06-24 22:41:28 -0300

    Register on 2021-06-24 22:41:28

--- frontend/src/pages/users/admin/addBookingSite/index.jsx
+++ frontend/src/pages/users/admin/addBookingSite/index.jsx
@@ -9,14 +9,30 @@ export default function AddBookingSite() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    async function registerBookingSite(){
-        try{
-            console.log('sao')
-        }catch(error){
+    async function registerBookingSite() {
+        try {
+            const data = {
+                email: email,
+                password: password,
+                url: url,
+            }
+            try {
+                await api.post('/bookingSite/register', data)
+                    .then(response => {
+                        if (response.status == 204) {
+                            alert('Site registrado com sucesso!')
+                        } else {
+                            alert('Erro ao cadastrar, verifique os campos e tente novamente')
+                        }
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+        } catch (error) {
             alert('Erro ao adicionar site de reservas, tente novamente.')
         }
     }
-    
+
     return (
         <div className="addBookingSite-pageContent">
             <Sidebar></Sidebar>
@@ -28,15 +44,15 @@ export default function AddBookingSite() {
                     <form id="registerForm" className="addBookingSite-filterForm" onSubmit={registerBookingSite}>
                         <div className="addBookingSite-filterInput">
                             <label htmlFor="bookingSiteInput">URL do site:</label>
-                            <input id="url" type="text" placeholder="URL" onChange={e=>setUrl(e.target.value)}/>
+                            <input id="url" type="text" placeholder="URL" onChange={e => setUrl(e.target.value)} />
                         </div>
                         <div className="addBookingSite-filterInput">
                             <label htmlFor="bookingSiteInput">Email:</label>
-                            <input id="email" type="text" placeholder="Email" pattern="[a-z]+([a-z]|.)*@[a-z]+\.([a-z]|.)*" title="Formato de email incorreto" onChange={e=>setEmail(e.target.value)}/>
+                            <input id="email" type="text" placeholder="Email" pattern="[a-z]+([a-z]|.)*@[a-z]+\.([a-z]|.)*" title="Formato de email incorreto" onChange={e => setEmail(e.target.value)} />
                         </div>
                         <div className="addBookingSite-filterInput">
                             <label htmlFor="bookingSiteInput">Senha:</label>
-                            <input id="password" type="password" placeholder="Senha" onChange={e=>setPassword(e.target.value)}/>
+                            <input id="password" type="password" placeholder="Senha" onChange={e => setPassword(e.target.value)} />
                         </div>
 
                         <input type="submit" value="REGISTRAR" />
--- frontend/src/pages/users/admin/addHotel/index.jsx
+++ frontend/src/pages/users/admin/addHotel/index.jsx
@@ -5,6 +5,31 @@ import Sidebar from '../../../../components/sidebar';
 import api from '../../../../services/api';
 
 export default function AddHotel() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [cnpj, setCnpj] = useState('')
+    const [name, setName] = useState('')
+    const [dailyRate, setDailyRate] = useState(0)
+    const [city, setCity] = useState('')
+
+    async function registerHotel(){
+        const data = {
+            email: email,
+            password: password,
+            name: name,
+            cnpj: cnpj,
+            city: city,
+            daily_rate: dailyRate,
+        }
+        await api.post('/hotel/register')
+            .then(response => {
+                if (response.status == 204) {
+                    alert('Hotel registrado com sucesso!')
+                } else {
+                    alert('Erro ao cadastrar, verifique os campos e tente novamente')
+                }
+            })
+    }
 
     return (
         <div className="addHotel-pageContent">
@@ -14,30 +39,30 @@ export default function AddHotel() {
                     <p>Registar Hotel</p>
                 </div>
                 <div className="addHotel-filter">
-                    <form className="addHotel-filterForm">
+                    <form className="addHotel-filterForm" onSubmit={registerHotel}>
                         <div id="registerForm" className="addHotel-filterInput">
                             <label htmlFor="hotelInput">Nome do Hotel:</label>
-                            <input id="hotelName" type="text" placeholder="Hotel" />
+                            <input id="hotelName" type="text" placeholder="Hotel" onChange={e=>setName(e.target.value)}/>
                         </div>
                         <div className="addHotel-filterInput">
                             <label htmlFor="hotelInput">CNPJ:</label>
-                            <input id="cnpj" type="text" placeholder="CNPJ" pattern="[0-9]{2}.[0-9]{3}.[0-9]{3}\/[0]{3}[1-2]-[0-9]" title="Formato de CNPJ incorreto (XX.XXX.XXX/000X-XXX)" />
+                            <input id="cnpj" type="text" placeholder="CNPJ" pattern="[0-9]{2}.[0-9]{3}.[0-9]{3}\/[0]{3}[1-2]-[0-9]" title="Formato de CNPJ incorreto (XX.XXX.XXX/000X-XXX)" onChange={e=>setCnpj(e.target.value)}/>
                         </div>
                         <div className="addHotel-filterInput">
                             <label htmlFor="hotelInput">Cidade:</label>
-                            <input id="city" type="text" placeholder="Cidade" />
+                            <input id="city" type="text" placeholder="Cidade" onChange={e=>setCity(e.target.value)}/>
                         </div>
                         <div className="addHotel-filterInput">
                             <label htmlFor="hotelInput">Email:</label>
-                            <input id="email" type="text" placeholder="Email" pattern="[a-z]+([a-z]|.)*@[a-z]+\.([a-z]|.)*" title="Formato de email incorreto" />
+                            <input id="email" type="text" placeholder="Email" pattern="[a-z]+([a-z]|.)*@[a-z]+\.([a-z]|.)*" title="Formato de email incorreto" onChange={e=>setEmail(e.target.value)}/>
                         </div>
                         <div className="addHotel-filterInput">
                             <label htmlFor="bookingSiteInput">Senha:</label>
-                            <input id="password" type="password" placeholder="Senha" />
+                            <input id="password" type="password" placeholder="Senha" onChange={e=>setPassword(e.target.value)}/>
                         </div>
                         <div className="addHotel-filterInput">
                             <label htmlFor="hotelInput">Preço da diária:</label>
-                            <input id="dailyRate" type="text" placeholder="Diária" />
+                            <input id="dailyRate" type="text" placeholder="Diária" onChange={e=>setDailyRate(e.target.value)}/>
                         </div>
                         <input type="submit" value="REGISTRAR" />
                     </form>
