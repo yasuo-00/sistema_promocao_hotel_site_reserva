bf55fce8c91ff59cb39260f4c91932ab55cdd773
yasuo-00 <matheusyt@hotmail.com>
2021-06-24 23:20:47 -0300

    AddSales Functionality on React on 2021-06-24 23:20:47

--- frontend/src/pages/addSales/index.jsx
+++ frontend/src/pages/addSales/index.jsx
@@ -5,11 +5,13 @@ import '../../assets/styles/addSales.css'
 import api from '../../services/api'
 
 export default function AddSales() {
-    const [initialDate, setInicialDate] = useState('')
+    const [initialDate, setInitialDate] = useState('')
     const [finalDate, setFinalDate] = useState('')
     const [price, setPrice] = useState('')
     const [hotelId, setHotelId] = useState('')
     const [bookingSiteId, setBookingSiteId] = useState('')
+    const [bookingSiteList, setBookingSiteList] = useState([])
+    const [hotelList, setHotelList] = useState([])
 
     async function submitSalesForm(e) {
         e.preventDefault()
@@ -30,6 +32,34 @@ export default function AddSales() {
             })
     }
 
+    async function bookingSiteSelect() {
+        await api.get('/bookingSite/listAll')
+            .then(response => {
+                if (response.status == 200) {
+                    console.log(response.data.bookingSiteList)
+                    setBookingSiteList(response.data.bookingSiteList)
+                }
+            })
+    }
+    async function hotelSelect() {
+        await api.get('/hotel/listAll')
+            .then(response => {
+                if (response.status == 200) {
+                    console.log(response.data.hotelList)
+                    setHotelList(response.data.hotelList)
+                }
+            })
+    }
+
+    useEffect(() => {
+        try {
+            bookingSiteSelect()
+            hotelSelect()
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
     return (
         <div class="pageContent">
             <Sidebar></Sidebar>
@@ -40,26 +70,40 @@ export default function AddSales() {
                 <div class="sales">
                     <form id="salesForm" class="salesForm" onSubmit={submitSalesForm}>
                         <div id="hotelSelect" class="salesInput">
+                            <label htmlFor="hotel">Hotel:</label>
+                            <select name="hotel" id="hotel" onChange={e=>setHotelId(e.target.value)}>
+                                {hotelList.map((hotel)=> (
+                                    <option value={hotel.id_user}>{hotel.name}</option>
+                                ))
+                                }
+                            </select>
                         </div>
                         <div id="bookingSiteSelect" class="salesInput">
+                            <label htmlFor="bookingSite">Site de Reservas:</label>
+                            <select name="bookingSite" id="bookingSite" onChange={e=>setBookingSiteId(e.target.value)}>
+                                {bookingSiteList.map((bookingSite)=> (
+                                    <option value={bookingSite.id_user}>{bookingSite.url}</option>
+                                ))
+                                }
+                            </select>
                         </div>
                         <div class="salesInput">
                             <div class="salesTimeframe">
                                 <div class="initialDate">
                                     <label htmlFor="initialDate">Data inicial:</label>
                                     <div class="calendarSvg"></div>
-                                    <input id="initialDate" type="date" placeholder="Data inicial" required />
+                                    <input id="initialDate" type="date" placeholder="Data inicial" required onChange={e => setInitialDate(e.target.value)} />
                                 </div>
                                 <div class="finalDate">
                                     <label htmlFor="finalDate">Data final:</label>
                                     <div class="calendarSvg"></div>
-                                    <input id="finalDate" type="date" placeholder="Data final" required />
+                                    <input id="finalDate" type="date" placeholder="Data final" required onChange={e => setFinalDate(e.target.value)} />
                                 </div>
                             </div>
                         </div>
                         <div class="salesInput">
                             <label htmlFor="priceInput">Preço:</label>
-                            <input type="number" id="priceInput" name="priceInput" placeholder="Preço" pattern="[0-9]{1,6}((.|,)[0-9]{1,2})?" title="Preço Inválido" required />
+                            <input type="number" id="priceInput" name="priceInput" placeholder="Preço" pattern="[0-9]{1,6}((.|,)[0-9]{1,2})?" title="Preço Inválido" required onChange={e => setPrice(e.target.value)} />
                         </div>
                         <input type="submit" value="ADICIONAR" />
                     </form>
